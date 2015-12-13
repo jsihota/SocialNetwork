3599dff5b56f0a1476f86c023bd7ef0f4e4e870f
jsihota <jagdeep_s1@hotmail.com>
2015-12-12 16:41:14 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + "/public"))
-app.use(bodyParser.json());
+app.use(express.static(__dirname + "/public"));
 
 app.listen(3001);
 console.log("Server running on port 3001");
